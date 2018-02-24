77f74ff048d5b173f889343411e88baded350876
André Hashiguchi <andre.hashiguchi@onclicksistemas.com.br>
2018-02-23 22:19:13 -0300

    Criação do projeto

---
+++ exercicios/middlewares.js
@@ -0,0 +1,26 @@
+//middleware pattern (hain of responsibility)
+// ... operador novo spread, sera passado um lista de parametros
+const exec = (ctx, ...middlewares) => {
+  
+  const run = index => {
+    middlewares && index < middlewares.length && middlewares[index](ctx, () => run (index + 1))
+  }
+
+  run(0);
+};
+
+const mid1 = (ctx, next) => {
+  ctx.info1 = 'mid1';
+  next();
+};
+
+const mid2 = (ctx, next) => {
+  ctx.info2 = 'mid2';
+  next();
+};
+
+const mid3 = ctx => ctx.info3 = 'mid3';
+
+const ctx = {};
+exec(ctx, mid1, mid2, mid3);
+console.log(ctx);
